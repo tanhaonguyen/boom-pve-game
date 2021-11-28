77ffda41bfb1086f9dd6b31f92d7e338779fad27
Nguyễn Tấn Hào <tanhaonguyen2015@gmail.com>
2021-11-28 21:57:19 +0700

    Fix player animation bug
    - Fix player animation bug.
    - Adjust obstacles prefab collider.
    - Adjust player collider.

--- assets/scripts/player_movement.ts
+++ assets/scripts/player_movement.ts
@@ -1,5 +1,5 @@
 
-import { _decorator, Component, Node, SystemEvent, systemEvent, EventKeyboard, KeyCode, Animation, TERRAIN_HEIGHT_BASE } from 'cc';
+import { _decorator, Component, Node, SystemEvent, systemEvent, EventKeyboard, KeyCode, Animation, TERRAIN_HEIGHT_BASE, RigidBody, Vec3, RigidBody2D, Vec2 } from 'cc';
 const { ccclass, property } = _decorator;
 
 /**
@@ -18,7 +18,7 @@ const { ccclass, property } = _decorator;
 export class PlayerMovement extends Component {
 
     private arrow_left_down: boolean = false;
-    private arrow_right_down:boolean = false;
+    private arrow_right_down: boolean = false;
     private arrow_up_down: boolean = false;
     private arrow_down_down: boolean = false;
     //------------------------------------------------------------------------------
@@ -41,7 +41,7 @@ export class PlayerMovement extends Component {
     update(deltaTime: number) {
     }
 
-    onDestroy(){
+    onDestroy() {
         systemEvent.off(SystemEvent.EventType.KEY_DOWN, this.onKeyPressed, this);
         systemEvent.off(SystemEvent.EventType.KEY_UP, this.onKeyReleased, this);
     }
@@ -49,49 +49,32 @@ export class PlayerMovement extends Component {
     onKeyPressed(event: EventKeyboard) {
         switch (event.keyCode) {
             case KeyCode.ARROW_LEFT:
-                if (!this.arrow_left_down){
+                if (!this.arrow_left_down) {
                     this.getComponent(Animation).play("player_left");
                     this.arrow_left_down = true;
-                    
                 }
-                this.arrow_right_down = false;
-                    this.arrow_up_down = false;
-                    this.arrow_down_down = false;
                 this.node.setPosition(this.node.position.x - this.moving_offset, this.node.position.y);
+                // this.getComponent(RigidBody2D).applyLinearImpulseToCenter(new Vec2(-5, 0), true);
                 break;
             case KeyCode.ARROW_RIGHT:
-                if (!this.arrow_right_down){
+                if (!this.arrow_right_down) {
                     this.getComponent(Animation).play("player_right");
-                   
                     this.arrow_right_down = true;
-                   
                 }
-                this.arrow_left_down =false;
-                this.arrow_up_down = false;
-                this.arrow_down_down = false;
                 this.node.setPosition(this.node.position.x + this.moving_offset, this.node.position.y);
                 break;
             case KeyCode.ARROW_UP:
-                if (!this.arrow_up_down){
+                if (!this.arrow_up_down) {
                     this.getComponent(Animation).play("player_up");
-                   
                     this.arrow_up_down = true;
                 }
-                this.arrow_left_down = false;
-                this.arrow_right_down = false;
-                this.arrow_down_down = false;
-
                 this.node.setPosition(this.node.position.x, this.node.position.y + this.moving_offset);
                 break;
             case KeyCode.ARROW_DOWN:
-                if (!this.arrow_down_down){
+                if (!this.arrow_down_down) {
                     this.getComponent(Animation).play("player_down");
-                  
                     this.arrow_down_down = true;
                 }
-                this.arrow_left_down = false;
-                this.arrow_right_down = false;
-                this.arrow_up_down = false;
                 this.node.setPosition(this.node.position.x, this.node.position.y - this.moving_offset);
                 break;
             case KeyCode.SPACE:
@@ -99,19 +82,39 @@ export class PlayerMovement extends Component {
         }
     }
 
-    onKeyReleased(event: EventKeyboard){
-        this.getComponent(Animation).stop();
+    onKeyReleased(event: EventKeyboard) {
+        let other_key_down_exist = false;
         switch (event.keyCode) {
             case KeyCode.ARROW_LEFT:
+                other_key_down_exist = this.arrow_right_down || this.arrow_up_down || this.arrow_down_down;
+                if (!other_key_down_exist){
+                    this.getComponent(Animation).play("player_idle_left");
+                }
+
                 this.arrow_left_down = false;
                 break;
             case KeyCode.ARROW_RIGHT:
+                other_key_down_exist = this.arrow_left_down || this.arrow_up_down || this.arrow_down_down;
+                if (!other_key_down_exist){
+                    this.getComponent(Animation).play("player_idle_right");
+                }
+
                 this.arrow_right_down = false;
                 break;
             case KeyCode.ARROW_UP:
+                other_key_down_exist = this.arrow_right_down || this.arrow_left_down || this.arrow_down_down;
+                if (!other_key_down_exist){
+                    this.getComponent(Animation).play("player_idle_up");
+                }
+
                 this.arrow_up_down = false;
                 break;
             case KeyCode.ARROW_DOWN:
+                other_key_down_exist = this.arrow_right_down || this.arrow_up_down || this.arrow_left_down;
+                if (!other_key_down_exist){
+                    this.getComponent(Animation).play("player_idle_down");
+                }
+
                 this.arrow_down_down = false;
                 break;
             case KeyCode.SPACE:
